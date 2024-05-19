3b3d3ce1428de52bfda9ba311510e33315740f39
Jaeyoung Lim <limjyjustin@gmail.com>
2024-05-19 23:13:45 +0900

    Update init.sql

--- k8s/configs/init.sql
+++ k8s/configs/init.sql
@@ -10,6 +10,6 @@ DROP TABLE IF EXISTS `coffee`;
 CREATE TABLE `coffee` (
     `id` int(11) NOT NULL AUTO_INCREMENT,
     `name` varchar(100),
-    `price` int(9)
+    `price` int(9),
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
